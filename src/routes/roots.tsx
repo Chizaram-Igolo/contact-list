251005d5c540a55bf31e5c8486c382c6bf89a8d9
Chizaram-Igolo <chizaram.igolo@yahoo.com>
2023-08-31 12:12:06 +0100

    ➕ Add script file (`contact-list.ts`) and styles file

--- src/routes/roots.tsx
+++ src/routes/roots.tsx
@@ -6,14 +6,31 @@ export default function Root() {
         <div>
           <form id="search-form" role="search">
             <input
-              type="text"
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
+              type="search"
+              name="q"
             />
+            <div id="search-spinner" aria-hidden hidden={true}></div>
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
           </form>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/contacts/1">Your Name</a>
+            </li>
+            <li>
+              <a href="/contacts/2">Your Friend</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <div id="detail"></div>
     </>
   );
 }
