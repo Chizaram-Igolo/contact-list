00686cfd6dfd7d0e92c41c3cb504def8a5b9e172
Chizaram-Igolo <chizaram@yahoo.com>
2023-09-04 03:44:38 +0100

    ⬆️⚒️ improvement: Enhance Search Functionality to search `onChange`, Add Search Spinner and Manage History Stack

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -16,6 +16,10 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
   useEffect(() => {
     (document.getElementById("q") as HTMLInputElement).value = q || "";
   }, [q]);
@@ -28,16 +32,18 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
               defaultValue={q || ""}
               onChange={(event) => {
-                submit(event.currentTarget.form);
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, { replace: !isFirstSearch });
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true}></div>
+            <div id="search-spinner" aria-hidden hidden={!searching}></div>
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
